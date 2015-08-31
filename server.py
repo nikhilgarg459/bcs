39a7424d995d8387c7fe7f45bff16e7b2c4a7833
Nikhil Garg <nikhilgarg72@gmail.com>
2015-08-31 21:39:37 +0530

    Adding base files

--- server.py
+++ server.py
@@ -1,15 +1,18 @@
-import socket               # Import socket module
+import socket               
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
+s = socket.socket()         
+host = socket.gethostname() 
+port = 12345                
+s.bind((host, port))        
 
-s.listen(5)                 # Now wait for client connection.
+s.listen(5)                 
 while True:
-   c, addr = s.accept()     # Establish connection with client.
+   c, addr = s.accept()     
    print 'Got connection from', addr
    c.send('Thank you for connecting')
    c.close()
 
-#Added by nishant garg
+
+
+
+
