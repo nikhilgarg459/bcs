ae4af3dd3d6398a9c0d4ac59df4fa1e9d47def7e
Nikhil Garg <nikhilgarg72@gmail.com>
2015-08-31 23:24:57 +0530

    comments added to client.py and server.py

--- client.py
+++ client.py
@@ -1,8 +1,13 @@
-import socket               
+#!usr/bin/env python
+#-*-coding:utf8-*-
+__doc__ = """
+This class provide client side access to application.
+"""
+import socket               # Import socket module
 
-s = socket.socket()         
-host = socket.gethostname() 
-port = 12345                
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345                # Reserve a port for your service.
 
 s.connect((host, port))
 print s.recv(1024)
