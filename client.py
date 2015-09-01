2e2ddbc23a08d91b24f6a1e02daa894e8f9e371e
Nikhil Garg <nikhilgarg72@gmail.com>
2015-09-01 23:41:21 +0530

    Connection settings updated

--- client.py
+++ client.py
@@ -5,43 +5,47 @@ This class provide client side access to application.
 """
 import socket               
 
-sock = socket.socket()         
-host = socket.gethostname() 
-port = 12344                
-sock.connect((host, port))
-print sock.recv(1024)     #Welcome to bcs
-recvd = sock.recv(1024)
-sock.send(raw_input(recvd))    #Username nikhil  #From emp.txt
-recvd = sock.recv(1024)
-sock.send(raw_input(recvd))    #Password nikhil   #From emp.txt  
-print sock.recv(1024)
-recvd=sock.recv(1024)
-if recvd == '1':                    #Login Successful   
-    while True:
-        recvd = sock.recv(1024)     #Get choices
-        sock.send(raw_input(recvd)) #Select Choice
-        recvd = sock.recv(1024)
-        if recvd == '3':
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         
+sock.connect((TCP_IP, TCP_PORT))
+try:
+    print sock.recv(1024)     #Welcome to bcs
+    recvd = sock.recv(1024)
+    sock.send(raw_input(recvd))    #Username nikhil  #From emp.txt
+    recvd = sock.recv(1024)
+    sock.send(raw_input(recvd))    #Password nikhil   #From emp.txt  
+    print sock.recv(1024)
+    recvd=sock.recv(1024)
+    if recvd == '1':                    #Login Successful   
+        while True:
+            recvd = sock.recv(1024)     #Get choices
+            sock.send(raw_input(recvd)) #Select Choice
             recvd = sock.recv(1024)
-            sock.send(raw_input(recvd)) #Create Userid:
-            recv = sock.recv(1024)
-            sock.send(raw_input(recvd)) #Create Password: 
-            recvd = sock.recv(1024)
-            sock.send(raw_input(recvd))  #Intial money: Rs
-            print sock.recv(1024)        #Account added successfully! # Added in custom.txt
-        elif recvd == '2':
-            recvd = sock.recv(1024)
-            sock.send(raw_input(recvd))  #Userid #Any Userid from custom.txt      
-            recvd = sock.recv(1024)
-            sock.send(raw_input(recvd))   #New Password: 
-            print sock.recv(1024)         #Password change successfully! || No user with username #in custom.txt
-        elif recvd == '1':
-            recvd = sock.recv(1024)
-            sock.send(raw_input(recvd))  #Userid to be deleted: # from custom.txt
-            print sock.recv(1024)        #Account deleted successfully!" || User not found in custom.txt
-        elif recvd == '4':
-            print sock.recv(1024)         #Logout Successful  
-            break
-        else:
-            print sock.recv(1024)         #Wrong Choice!
-sock.close()
+            if recvd == '3':
+                recvd = sock.recv(1024)
+                sock.send(raw_input(recvd)) #Create Userid:
+                recv = sock.recv(1024)
+                sock.send(raw_input(recvd)) #Create Password: 
+                recvd = sock.recv(1024)
+                sock.send(raw_input(recvd))  #Intial money: Rs
+                print sock.recv(1024)        #Account added successfully! # Added in custom.txt
+            elif recvd == '2':
+                recvd = sock.recv(1024)
+                sock.send(raw_input(recvd))  #Userid #Any Userid from custom.txt      
+                recvd = sock.recv(1024)
+                sock.send(raw_input(recvd))   #New Password: 
+                print sock.recv(1024)         #Password change successfully! || No user with username #in custom.txt
+            elif recvd == '1':
+                recvd = sock.recv(1024)
+                sock.send(raw_input(recvd))  #Userid to be deleted: # from custom.txt
+                print sock.recv(1024)        #Account deleted successfully!" || User not found in custom.txt
+            elif recvd == '4':
+                print sock.recv(1024)         #Logout Successful  
+                break
+            else:
+                print sock.recv(1024)         #Wrong Choice!
+except Exception, e:
+    print e.args                        
+finally:    
+    sock.close()
--- server.py
+++ server.py
@@ -6,60 +6,62 @@ This class provide server side access to application.
 from employee import Employee
 import socket               
 
-sock = socket.socket()         
-host = socket.gethostname() 
-port = 12344               
-sock.bind((host, port))        
-
-sock.listen(5)                
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)              
+sock.bind((TCP_IP, TCP_PORT))
+sock.listen(1)                
 client, addr = sock.accept()     # Establish connection with client.
 print 'Got connection from', addr
-client.send('Welcome to bcs')
-client.send('Username: ') #nikhil
-user = client.recv(1024)
-client.send('Password: ')	#nikhil
-passw = client.recv(1024)
-with open ("emp.txt", "r") as myfile:
-	data = myfile.read()
-	attribs = data.split(' ')
-	if(user == attribs[0] and passw == attribs[1]):
-		client.send('Login successful!')
-		client.send('1')
-		empl = Employee(user,passw)
-		while True:
-			client.send('1 Add new Account\n2 Delete Account\n3 Change Password\n4 Logout\nPlease enter ypur choice: ')
-			choice = client.recv(1024)
-			if choice == '1':
-				client.send('3')
-				client.send('Create Userid: ')
-				username = client.recv(1024)
-				client.send("Create Password: ")
-				password = client.recv(1024)
-				client.send("Intial money: Rs")
-				money = client.recv(1024)
-				client.send(empl.add_Account(username,password,money))
-			elif choice == '2':
-				client.send('1')
-				client.send('Userid to be deleted: ')
-				username = client.recv(1024)
-				client.send(empl.delete_Account(username))
-			elif choice == '3':
-				client.send('2')
-				client.send('Userid: ')
-				username = client.recv(1024)
-				client.send("New Password: ")
-				password = client.recv(1024)
-				client.send(empl.change_Password(username,password))
-			elif choice =='4':
-				client.send('4')
-				client.send('Logout Successful')
-				break
-			else:
-				client.send('5')
-				client.send("Wrong Choice!")				
-	else:	
-		client.send('Wrong username or password!')
-		client.send('0')
-client.close()    
-sock.close()
+try:
+	client.send('Welcome to bcs')
+	client.send('Username: ') #nikhil
+	user = client.recv(1024)
+	client.send('Password: ')	#nikhil
+	passw = client.recv(1024)
+	with open ("emp.txt", "r") as myfile:
+		data = myfile.read()
+		attribs = data.split(' ')
+		if(user == attribs[0] and passw == attribs[1]):
+			client.send('Login successful!')
+			client.send('1')
+			empl = Employee(user,passw)
+			while True:
+				client.send('1 Add new Account\n2 Delete Account\n3 Change Password\n4 Logout\nPlease enter ypur choice: ')
+				choice = client.recv(1024)
+				if choice == '1':
+					client.send('3')
+					client.send('Create Userid: ')
+					username = client.recv(1024)
+					client.send("Create Password: ")
+					password = client.recv(1024)
+					client.send("Intial money: Rs")
+					money = client.recv(1024)
+					client.send(empl.add_Account(username,password,money))
+				elif choice == '2':
+					client.send('1')
+					client.send('Userid to be deleted: ')
+					username = client.recv(1024)
+					client.send(empl.delete_Account(username))
+				elif choice == '3':
+					client.send('2')
+					client.send('Userid: ')
+					username = client.recv(1024)
+					client.send("New Password: ")
+					password = client.recv(1024)
+					client.send(empl.change_Password(username,password))
+				elif choice =='4':
+					client.send('4')
+					client.send('Logout Successful')
+					break
+				else:
+					client.send('5')
+					client.send("Wrong Choice!")				
+		else:	
+			client.send('Wrong username or password!')
+			client.send('0')
+except Exception, e:
+    print e.args 
+finally:			
+	client.close()    
   
